bb8696bdfeb6a465902ebdc311a90334ed61c01f
Marcus Vinicius <buba.sergipe@gmail.com>
2022-01-05 21:55:44 -0300

    Test format function in api-error

--- src/controllers/articles.ts
+++ src/controllers/articles.ts
@@ -2,6 +2,7 @@ import { ArticleService } from '@src/services/article';
 import { Controller, Get, Post, Put, Delete } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { BaseController } from './index';
+import { ArticleI } from '../models/Articles';
 
 const services = new ArticleService();
 
@@ -11,12 +12,10 @@ export class ArticlesController extends BaseController {
   public async getArticles(req: Request, res: Response): Promise<void> {
     const page = parseInt(req.query.page as string) || 1;
     const limit = parseInt(req.query.limit as string) || 10;
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
 
     try {
       const articles = await services.getArticlesFromDB();
-      const results = articles.slice(startIndex, endIndex);
+      const results = this.pagination<ArticleI>(page, limit, articles);
       res.status(200).json(results);
     } catch (error) {
       this.sendErrorResponse(res, {
@@ -25,6 +24,13 @@ export class ArticlesController extends BaseController {
       });
     }
   }
+
+  private pagination<T>(page: number, limit: number, data: T[]): T[] {
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    return data.slice(startIndex, endIndex);
+  }
+
   @Post('')
   public async createArticle(req: Request, res: Response): Promise<void> {
     try {
